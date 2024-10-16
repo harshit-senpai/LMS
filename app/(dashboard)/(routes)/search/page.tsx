967b58b165d8e67e0e30d101a978f635e04721db
harshit Mishra <miananya2@gmail.com>
2024-10-17 03:44:57 +0530

    Search query

---
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -0,0 +1,21 @@
+import { db } from "@/lib/db";
+import { Categories } from "./_components/Categories";
+import { SearchInput } from "@/components/SearchInput";
+
+export default async function SearchPage() {
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+  return (
+    <>
+      <div className="px-6 pt-6 md:hidden md:mb-0 block">
+        <SearchInput />
+      </div>
+      <div className="p-6">
+        <Categories items={categories} />
+      </div>
+    </>
+  );
+}
--- components/NavbarRoutes.tsx
+++ components/NavbarRoutes.tsx
@@ -5,30 +5,41 @@ import { usePathname } from "next/navigation";
 import { Button } from "./ui/button";
 import { LogOut } from "lucide-react";
 import Link from "next/link";
+import { SearchInput } from "./SearchInput";
 
 export const NavbarRoutes = () => {
   const pathName = usePathname();
 
   const isTeacherPage = pathName?.startsWith("/teacher");
   const isPlayerPage = pathName?.startsWith("/player");
+  const isSearchPage = pathName?.startsWith("/search");
 
   return (
-    <div className="flex gap-x-2 ml-auto">
-      {isTeacherPage || isPlayerPage ? (
-        <Link href={"/"}>
-          <Button size={"sm"} variant={"ghost"}>
-            <LogOut className="h-4 w-4 mr-2" />
-            Exit
-          </Button>
-        </Link>
-      ) : (
-        <Link href={"/teacher/courses"}>
-          <Button size={"sm"} variant={"ghost"}>
-            Teacher Mode
-          </Button>
-        </Link>
-      )}
-      <UserButton />
-    </div>
+    <>
+    {
+      isSearchPage && (
+        <div className="hidden md:block">
+          <SearchInput/>
+        </div>
+      )
+    }
+      <div className="flex gap-x-2 ml-auto">
+        {isTeacherPage || isPlayerPage ? (
+          <Link href={"/"}>
+            <Button size={"sm"} variant={"ghost"}>
+              <LogOut className="h-4 w-4 mr-2" />
+              Exit
+            </Button>
+          </Link>
+        ) : (
+          <Link href={"/teacher/courses"}>
+            <Button size={"sm"} variant={"ghost"}>
+              Teacher Mode
+            </Button>
+          </Link>
+        )}
+        <UserButton />
+      </div>
+    </>
   );
 };
