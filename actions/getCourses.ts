a9306d077a1d25b1b36ced1ff6a09c396d0af3f6
harshit Mishra <miananya2@gmail.com>
2024-10-18 05:29:09 +0530

    Course & Course layout

---
+++ actions/getCourses.ts
@@ -0,0 +1,79 @@
+"use server";
+
+import { db } from "@/lib/db";
+import { Category, Course } from "@prisma/client";
+import { getProgress } from "./getProgress";
+
+type CoursesWithProgressWithCategory = Course & {
+  category: Category | null;
+  chapters: { id: string }[];
+  progress: number | null;
+};
+
+type GetCourses = {
+  userId: string;
+  title?: string;
+  categoryId?: string;
+};
+
+export const getCourses = async ({
+  userId,
+  title,
+  categoryId,
+}: GetCourses): Promise<CoursesWithProgressWithCategory[]> => {
+  try {
+    const courses = await db.course.findMany({
+      where: {
+        isPublished: true,
+        title: {
+          contains: title,
+        },
+        categoryId,
+      },
+      include: {
+        Category: true,
+        chapters: {
+          where: {
+            isPublished: true,
+          },
+          select: {
+            id: true,
+          },
+        },
+        purchases: {
+          where: {
+            userId,
+          },
+        },
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    const coursesWithProgress: CoursesWithProgressWithCategory[] =
+      await Promise.all(
+        courses.map(async (course) => {
+          if (course.purchases.length === 0) {
+            return {
+              ...course,
+              progress: null,
+              category: course.Category,
+            };
+          }
+          const progressPercentage = await getProgress(userId, course.id);
+
+          return {
+            ...course,
+            category: course.Category,
+            progress: progressPercentage,
+          };
+        })
+      );
+
+    return coursesWithProgress;
+  } catch (error) {
+    console.log("[GET_COURSES]", error);
+    return [];
+  }
+};
--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,20 +1,44 @@
 import { db } from "@/lib/db";
 import { Categories } from "./_components/Categories";
 import { SearchInput } from "@/components/SearchInput";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { CoursesList } from "../../../../components/CourseList";
+import { getCourses } from "@/actions/getCourses";
 
-export default async function SearchPage() {
+interface SearchPageProps {
+  searchParams: {
+    title: string;
+    categoryId: string;
+  };
+}
+
+export default async function SearchPage({ searchParams }: SearchPageProps) {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
   const categories = await db.category.findMany({
     orderBy: {
       name: "asc",
     },
   });
+
+  const courses = await getCourses({
+    userId,
+    ...searchParams,
+  });
+
   return (
     <>
       <div className="px-6 pt-6 md:hidden md:mb-0 block">
         <SearchInput />
       </div>
-      <div className="p-6">
+      <div className="p-6 space-y-8">
         <Categories items={categories} />
+        <CoursesList items={courses} />
       </div>
     </>
   );
